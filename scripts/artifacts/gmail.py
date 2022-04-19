215d5e7eb3eb174f199a8e94f8ff5c8c03b3670b
kush2312 <kushdabade23@gmail.com>
2022-04-19 16:46:42 +0530

    first commit

--- scripts/artifacts/gmail.py
+++ scripts/artifacts/gmail.py
@@ -9,24 +9,6 @@ import xml.etree.ElementTree as ET
 
 from scripts.artifact_report import ArtifactHtmlReport
 from scripts.ilapfuncs import logfunc, tsv, timeline, is_platform_windows, open_sqlite_db_readonly, does_table_exist
-import sys
-import os
-import time
-import re
-import hashlib
-import sqlite3 as sq
-from json import loads
-from binascii import hexlify
-from datetime import datetime
-from datetime import timedelta
-from subprocess import check_output as co
-from subprocess import call
-
-# Check OS and define adb
-download_adb = ' ERROR! \n\'./adb\' file is not present!\n Unzip, and place them into this directory;\n Run the program again.'
-
-ADB = "adb.exe"
-SEP = '\\'
 
 class keyboard_event:
     def __init__(self, id, app, text, textbox_name, textbox_id, event_date, start_date='', end_date=''):
@@ -41,27 +23,15 @@ class keyboard_event:
 
 def get_gmailActive(files_found, report_folder, seeker, wrap_text):
     #logfunc("If you can read this, the module is working!")
-    logfunc("Function Called")
-    # A version
-    BUILDPROP = co([ADB, 'shell', 'cat', '/system/build.prop']).decode('UTF-8')
-    for aver in BUILDPROP.split('\n'):
-        if 'ro.build.version.release' in aver:
-            ANDROID_VER = aver.strip().split('=')[1]
-    # try:
-    #     print(" Android version: " + ANDROID_VER); REPORT.append(["Android version", ANDROID_VER])
-    # except:
-    #     pass
-
-    logfunc(ANDROID_VER)
-    activeAccount = ANDROID_VER
-    file_found = "kush/home"
-    # file_found = str(files_found[0])
-    # xmlTree = ET.parse(file_found)
-    # root = xmlTree.getroot()
-    # for child in root:
-    #     if child.attrib['name'] == "active-account":
-    #         logfunc("Active gmail account found: " + child.text)
-    #         activeAccount = child.text
+    #logfunc(files_found)
+    activeAccount = ''
+    file_found = str(files_found[0])
+    xmlTree = ET.parse(file_found)
+    root = xmlTree.getroot()
+    for child in root:
+        if child.attrib['name'] == "active-account":
+            logfunc("Active gmail account found: " + child.text)
+            activeAccount = child.text
 
     if activeAccount != '':
         report = ArtifactHtmlReport('Gmail - Active')
